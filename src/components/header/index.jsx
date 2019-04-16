18b2ab99a6585f808593f703a3aa763da6384047
Denis <disamapm@gmail.com>
2019-04-16 15:45:29 +0300

    Installs react-router for routing

---
+++ src/components/header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class HeaderContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return;
+  }
+}
+
+export default HeaderContainer;
